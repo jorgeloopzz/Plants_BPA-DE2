a1b5a835be59c31b09c25a64b523563ce8b3cbf0
jorgeloopzz <jorgeloopzz@protonmail.com>
2024-11-29 19:31:49 +0000

    Define each color in functions

--- src/leds.py
+++ src/leds.py
@@ -2,55 +2,54 @@ from machine import Pin
 from neopixel import NeoPixel
 from time import sleep
 
-pixels = NeoPixel(Pin(15), 16)
+n=20
+pixels = NeoPixel(Pin(25), n)
 
 def off():
   off = (0, 0, 0) 
-
-  for i in range(16):
+  for i in range(n):
     pixels[i] = off
   pixels.write()
   sleep(0.1)
   
-
 def blue():
   azul = (0, 102, 255) 
-  for i in range(16):
+  for i in range(n):
     pixels[i] = azul
   pixels.write()
   sleep(0.1)
 
 def red():
   rojo = (255, 0, 0) 
-  for i in range(16):
+  for i in range(n):
     pixels[i] = rojo
   pixels.write()
   sleep(0.1)
 
 def uv():
   purple = (153, 51, 255) 
-  for i in range(16):
+  for i in range(n):
     pixels[i] = purple
   pixels.write()
   sleep(0.1)
 
 def white():
   white = (255, 255, 255) 
-  for i in range(16):
+  for i in range(n):
     pixels[i] = white
   pixels.write()
   sleep(0.1)
 
 def yellow():
   yellow = (255, 255, 0) 
-  for i in range(16):
+  for i in range(n):
     pixels[i] = yellow
   pixels.write()
   sleep(0.1)
 
 def green():
   green = (0, 204, 0) 
-  for i in range(16):
+  for i in range(n):
     pixels[i] = green
   pixels.write()
   sleep(0.1)
